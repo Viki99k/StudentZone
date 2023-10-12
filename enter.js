4c40a0d11656bf07c63163731af91cad8abd483a
Vikrant Kulkarni <104883815+Viki99k@users.noreply.github.com>
2023-10-13 01:21:33 +0530

    Update enter.js

--- enter.js
+++ enter.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const getActivityButton = document.getElementById("getActivityButton");
     const activityResult = document.getElementById("activityResult");
 
-      const getJokeButton = document.getElementById("getJokeButton");
-    const jokeText = document.getElementById("jokeText");
-
     getJokeButton.addEventListener("click", function () {
         fetch("https://v2.jokeapi.dev/joke/Any")
             .then(response => response.json())
@@ -22,15 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error("Error:", error);
                 activityResult.textContent = "Unable to fetch a joke. Please try again.";
             });
-    });
-});
-This code adds a "Get Joke" button and a result paragraph for displaying jokes fetched from the Unofficial JokeAPI. When you click the "Get Joke" button, it fetches a random joke from the API and displays it in the result paragraph. Don't forget to style the "jokeResult" element to make the joke text visually appealing.
-
-
-
-
-
-
     });
 });
 
