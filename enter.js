f13bcb66fa27575eb8e229905b085efe7a182b1d
Vikrant Kulkarni <104883815+Viki99k@users.noreply.github.com>
2023-10-13 01:01:13 +0530

    Update enter.js

--- enter.js
+++ enter.js
@@ -3,22 +3,28 @@ document.addEventListener("DOMContentLoaded", function () {
     const activityResult = document.getElementById("activityResult");
 
     getActivityButton.addEventListener("click", function () {
-        fetch("https://api.adviceslip.com/advice/")
+        fetch("https://api.adviceslip.com/advice")
             .then(response => response.json())
             .then(data => {
-                const adviceText = data.slip.advice;
-                activityResult.textContent = "Advise: " + adviceText;
-                activityResult.classList.remove("error");
-                activityResult.classList.add("success");
+                if (data && data.slip && data.slip.advice) {
+                    const adviceText = data.slip.advice;
+                    activityResult.textContent = "Advice: " + adviceText;
+                    activityResult.classList.remove("error");
+                    activityResult.classList.add("success");
+                } else {
+                    console.error("Error: Unable to fetch advice data");
+                    activityResult.textContent = "Unable to fetch advice. Please try again.";
+                    activityResult.classList.remove("success");
+                    activityResult.classList.add("error");
+                }
             })
             .catch(error => {
                 console.error("Error:", error);
-                activityResult.textContent = "Unable to fetch activity. Please try again.";
+                activityResult.textContent = "Unable to fetch advice. Please try again.";
                 activityResult.classList.remove("success");
                 activityResult.classList.add("error");
             });
     });
-
-   
 });
+
 
