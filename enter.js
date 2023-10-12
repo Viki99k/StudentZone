b2defab3446794701f48229d58e37c7f6f889d13
Vikrant Kulkarni <104883815+Viki99k@users.noreply.github.com>
2023-10-13 00:17:15 +0530

    Add files via upload

---
+++ enter.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const getActivityButton = document.getElementById("getActivityButton");
+    const activityResult = document.getElementById("activityResult");
+
+    getActivityButton.addEventListener("click", function () {
+        fetch("http://www.boredapi.com/api/activity/")
+            .then(response => response.json())
+            .then(data => {
+                const activityText = data.activity;
+                activityResult.textContent = "Activity: " + activityText;
+                activityResult.classList.remove("error");
+                activityResult.classList.add("success");
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                activityResult.textContent = "Unable to fetch activity. Please try again.";
+                activityResult.classList.remove("success");
+                activityResult.classList.add("error");
+            });
+    });
+
+   
+});
+
