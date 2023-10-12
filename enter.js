5e9ed5d98d2e6abf8fd3992e1837ac264c160340
Vikrant Kulkarni <104883815+Viki99k@users.noreply.github.com>
2023-10-13 01:16:12 +0530

    Update enter.js

--- enter.js
+++ enter.js
@@ -2,28 +2,35 @@ document.addEventListener("DOMContentLoaded", function () {
     const getActivityButton = document.getElementById("getActivityButton");
     const activityResult = document.getElementById("activityResult");
 
-    getActivityButton.addEventListener("click", function () {
-        fetch("https://api.adviceslip.com/advice")
+      const getJokeButton = document.getElementById("getJokeButton");
+    const jokeText = document.getElementById("jokeText");
+
+    getJokeButton.addEventListener("click", function () {
+        fetch("https://v2.jokeapi.dev/joke/Any")
             .then(response => response.json())
             .then(data => {
-                if (data && data.slip && data.slip.advice) {
-                    const adviceText = data.slip.advice;
-                    activityResult.textContent = "Advice: " + adviceText;
-                    activityResult.classList.remove("error");
-                    activityResult.classList.add("success");
+                if (data.setup && data.delivery) {
+                   activityResult.textContent = `${data.setup}\n${data.delivery}`;
+                } else if (data.joke) {
+                    activityResult.textContent = data.joke;
                 } else {
-                    console.error("Error: Unable to fetch advice data");
-                    activityResult.textContent = "Unable to fetch advice. Please try again.";
-                    activityResult.classList.remove("success");
-                    activityResult.classList.add("error");
+                    console.error("Error: Unable to fetch joke data");
+                   activityResult.textContent = "Unable to fetch a joke. Please try again.";
                 }
             })
             .catch(error => {
                 console.error("Error:", error);
-                activityResult.textContent = "Unable to fetch advice. Please try again.";
-                activityResult.classList.remove("success");
-                activityResult.classList.add("error");
+                activityResult.textContent = "Unable to fetch a joke. Please try again.";
             });
+    });
+});
+This code adds a "Get Joke" button and a result paragraph for displaying jokes fetched from the Unofficial JokeAPI. When you click the "Get Joke" button, it fetches a random joke from the API and displays it in the result paragraph. Don't forget to style the "jokeResult" element to make the joke text visually appealing.
+
+
+
+
+
+
     });
 });
 
